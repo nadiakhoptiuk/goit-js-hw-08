1c0c928767b786f26d67b06dfd814dcc032b2530
nadiakhoptiuk <98387901+nadiakhoptiuk@users.noreply.github.com>
2022-06-02 15:22:21 +0000

    Deploying to gh-pages from @ nadiakhoptiuk/goit-js-hw-08@d9e32c4ef0699e1c13ac27f724c605cd9c7c114b 🚀

--- 02-video.09323393.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("iframe"),o=new Vimeo.Player(e);console.dir(e),o.on("play",(function(){console.log("played the video!")})),o.getVideoTitle().then((function(e){console.log("title:",e)}));
-//# sourceMappingURL=02-video.09323393.js.map
