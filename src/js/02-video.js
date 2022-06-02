d9e32c4ef0699e1c13ac27f724c605cd9c7c114b
Nadia Khoptiuk <holineinadia@gmail.com>
2022-06-02 18:21:20 +0300

    add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,36 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
-console.dir(iframe);
+const STORAGE_KEY = 'videoplayer-current-time';
 
-player.on('play', function () {
-  console.log('played the video!');
-});
+refreshCurrentPlayerTime();
 
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+//додаємо слухач на подію timeupdate
+player.on('timeupdate', throttle(loadTimeUpdate, 1000));
+
+//функція, яка отримує час відтворення
+function loadTimeUpdate({ seconds }) {
+  addToStorageUpdatedTime(seconds);
+}
+
+// функція, яка записує час відтворення в локальне сховище
+function addToStorageUpdatedTime(time) {
+  localStorage.setItem(STORAGE_KEY, time);
+}
+
+//функція, яка перевіряє наявність збереженого часу
+function hasStorageCurrentTime() {
+  return localStorage.getItem(STORAGE_KEY) ?? false;
+}
+
+// функція, яка встановлює поточний час після перезавантаження сторінки
+function refreshCurrentPlayerTime() {
+  if (!hasStorageCurrentTime()) {
+    return;
+  }
+
+  player.setCurrentTime(hasStorageCurrentTime());
+}
